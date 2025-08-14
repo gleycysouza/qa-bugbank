de7e39cdfd37197f0962376948d8dd2316f9c3ac
Gleycy-Kelly <gleycy.ksouza@gmail.com>
2025-08-13 23:31:43 -0300

    Ajuste das variaveis de relatorio para envio de log e unificacao com a pipeline

--- cypress.config.ts
+++ cypress.config.ts
@@ -16,10 +16,13 @@ export default defineConfig({
     },
     reporter: '@reportportal/agent-js-cypress',
     reporterOptions: {
-        endpoint: process.env.REPORT_PORTAL_API_URL,
-        apiKey: process.env.REPORT_PORTAL_API_TOKEN,
-        launch: process.env.REPORT_PORTAL_LAUNCH_NAME,
+        endpoint: process.env.REPORT_PORTAL_API_URL,      
+        apiKey: process.env.REPORT_PORTAL_API_TOKEN,      
+        launch: process.env.REPORT_PORTAL_LAUNCH_NAME,   
         project: process.env.REPORT_PORTAL_PROJECT_NAME,
+        autoMerge: true, 
+        rerun: true,                                     
+        rerunOfLaunch: process.env.REPORT_PORTAL_ID_REEXECUCAO, 
         attributes: [{ key: 'branch', value: 'master' }]
     }
 })
